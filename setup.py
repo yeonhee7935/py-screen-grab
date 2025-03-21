2eb2184c2a35f32b74edca3b4939e060860fb86c
yeonhee7935 <yeonhee7935@naver.com>
2025-03-21 17:24:41 +0900

    refactor: setup dependencies in ci

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Operating System :: POSIX :: Linux', 
     ],
     python_requires=">=3.6",
     test_suite="tests",
