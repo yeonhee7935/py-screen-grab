42f4a7a6457d8181fb4b01a250082e9f019e5228
yeonhee7935 <yeonhee7935@naver.com>
2025-03-26 11:07:31 +0900

    refactor: commit for ci/cd

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+import sys
 from setuptools import setup, find_packages
 
+try:
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError as e:
+    print(e)
+    pass
 
 __version__ = "1.2.0"
     
 setup(
-    name="py-screen-grab",
+    name="py-screen-grab", 
     version=__version__,
     packages=find_packages(exclude=["tests", "tests.*", "examples"]),
     install_requires=[
