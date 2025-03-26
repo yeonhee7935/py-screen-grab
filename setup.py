c3906f6c5dce43a45200b74b5340fcf0e66d6e0d
yeonhee7935 <yeonhee7935@naver.com>
2025-03-26 11:35:57 +0900

    refactor: commit for ci/cd

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
+import sys
 from setuptools import setup, find_packages
 
+try:
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError as e:
+    print(e)
+    pass
 
 __version__ = "1.2.0"
     
