e7fc62e21286c0b983d961444374671c21223d67
yeonhee7935 <yeonhee7935@naver.com>
2025-04-25 10:00:14 +0900

    chore: Update dependencies for GStreamer

---
+++ requirements.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0
+
+# Gst
+sudo apt install gir1.2-gstreamer-1.0 gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good
